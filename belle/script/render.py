43cb6b34f29c4c4f72fe2310015b6571ee1b4979
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2011-12-08 13:41:41 +0900

    Exiting with code 127 on unknown command line

--- belle/script/render.py
+++ belle/script/render.py
@@ -58,3 +58,5 @@ if __name__ == '__main__':
         render(sys.argv[2])
     elif mode == 'generate-thumbnail':
         generate_thumbnail(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
+    else:
+        sys.exit(127)
