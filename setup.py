f64ae585f4ea9bd3240684c7259069e474e6a3df
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2012-01-20 15:21:50 +0900

    Bumping version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if sys.version_info[:3] < (2,5,0):
 
 setup(
     name="belle",
-    version='0.1.7',
+    version='0.1.8',
     description="belle, renderer",
     long_description="""""",
     classifiers=[
